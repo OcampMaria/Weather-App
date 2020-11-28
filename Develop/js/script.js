79c87d9d261387cb2bb90b65b0bfce66603200b9
Maria Ocampo <mariaocampo0513@gmail.com>
2020-11-27 19:59:10 -0800

    Modified styling, addded 5-day Forecast section.

---
+++ Develop/js/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function (){
+  $("#search").click(function(){
+    
+    var cityName = $(".inputCity").val();
+    var WeatherAPI= "https://api.openweathermap.org/data/2.5/weather?q=London&appid=c600d0cc510e3815c07a6df7cf76f58e";
+    
+    if (cityName != ""){
+      $.ajax({
+        url: WeatherAPI,
+        method: "GET",
+        success: function (data) {
+          console.log(data);
+        }
+
+      })
+
+
+    } else {
+      $("#error").html("Field cannot be empty");
+      console.log();
+    }
+  
+  
+    
+  });
+});

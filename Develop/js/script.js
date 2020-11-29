9cc3cfe925467ec3e8e5e76138f65050a5c2b6d6
Maria Ocampo <mariaocampo0513@gmail.com>
2020-11-28 21:14:49 -0800

    Added icon

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -2,8 +2,8 @@ $(document).ready(function (){
   $("#search").click(function(){
     
     var cityName = $(".inputCity").val();
-    var WeatherAPI= "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units" + "&appid=c6fd3ae4f6763936ec10d5be134b091f";
-  
+    var WeatherAPI= "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=metric" + "&appid=c6fd3ae4f6763936ec10d5be134b091f";
+    var fiveDayAPI= "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=c6fd3ae4f6763936ec10d5be134b091f";
     
     
     if (cityName != ""){
@@ -16,40 +16,41 @@ $(document).ready(function (){
           var widget = show(data)
           $("#outputWeather").html(widget);
           $("#inputCity").val("")
-        }    
+        }
+
       });
 
       // prepend buttons for searched cities
       $( ".prepend" ).append ("<div>" + "<button class='button is-fullwidth' >" + cityName + "</button>" + "</div>");
 
+      fiveDayForewast()
+
     } else{
       $("#error").html("field cannot be empty")
     }
       
-
-
-
-
-
-
-
-
-
-
-
   });
 });
 
 function show (data) {
   return "<div class='cityInfo sections' id='outputWeather'" +
-  "<h1 style='font-weight:bold;'> <img class='image is-48x48' src= http://openweathermap.org/img/w/" + data.weather[0].icon + ".png>" + data.name + "</h1>" +
+  "<h1 style='font-weight:bold; font-size:30px'>"+ "<div class= 'cityweather'>" + data.name + "<img class='image' src= http://openweathermap.org/img/w/" + data.weather[0].icon + ".png>" + "</div>" + "</h1>" +
 
-    "<h3>Temperature: " + data.main.temp + "&deg;C</h3>" +
-    "<h3>Humidity: " + data.main.humidity + "%</h3>" +
-    "<h3>Wind Speed: " + data.wind.speed + "m/s</h3>" +
-    "<h3>UV index: " + "</h3>" +
-    "</div>" 
+    "<h3 class='font'>Temperature: " + data.main.temp + "&deg;C</h3>" +
+    "<h3 class='font'>Humidity: " + data.main.humidity + "%</h3>" +
+    "<h3 class='font'>Wind Speed: " + data.wind.speed + "m/s</h3>" +
+    "<h3 class='font'>UV index: " + "</h3>" +
+    "</div>";
+    
+  
 }
+
+// function fiveDayForewast (){
+//   $.ajax({
+//     url: fiveDayAPI,
+//     method: "GET",
+// } 
+// .then(function)
 
 // "<div class='fiveDay'>" + 
 //     "<p><strong>5-Day Forecast</strong> </p>" +

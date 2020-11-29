0b79cdeb9f447b9ebfd6a6dcc3c13cecea8aee75
Maria Ocampo <mariaocampo0513@gmail.com>
2020-11-29 13:05:58 -0800

    Added a second API for the five day forecast

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,11 +1,12 @@
+var apiKey = "c6fd3ae4f6763936ec10d5be134b091f"
+
 $(document).ready(function (){
   $("#search").click(function(){
     
     var cityName = $(".inputCity").val();
     var WeatherAPI= "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=metric" + "&appid=c6fd3ae4f6763936ec10d5be134b091f";
     var fiveDayAPI= "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=c6fd3ae4f6763936ec10d5be134b091f";
-    
-    
+        
     if (cityName != ""){
       $.ajax({
         url: WeatherAPI,
@@ -13,22 +14,33 @@ $(document).ready(function (){
         success: function (data) {
           console.log(data);  
           
-          var widget = show(data)
-          $("#outputWeather").html(widget);
+          var current = show(data)
+          $("#outputWeather").html(current);
           $("#inputCity").val("")
         }
-
       });
-
       // prepend buttons for searched cities
       $( ".prepend" ).append ("<div>" + "<button class='button is-fullwidth' >" + cityName + "</button>" + "</div>");
 
-      fiveDayForewast()
+      
+      $.ajax({
+        url: fiveDayAPI,
+        method: "GET",
+        success: function (data) {
+          console.log(data);  
+
+          var forecast = display(data)
+          $(".forecast").html(forecast);
+          
+        }
+      });
+
+     
 
     } else{
       $("#error").html("field cannot be empty")
     }
-      
+
   });
 });
 
@@ -40,23 +52,21 @@ function show (data) {
     "<h3 class='font'>Humidity: " + data.main.humidity + "%</h3>" +
     "<h3 class='font'>Wind Speed: " + data.wind.speed + "m/s</h3>" +
     "<h3 class='font'>UV index: " + "</h3>" +
-    "</div>";
-    
-  
+    "</div>";  
+    display(data)
 }
 
-// function fiveDayForewast (){
-//   $.ajax({
-//     url: fiveDayAPI,
-//     method: "GET",
-// } 
-// .then(function)
+function display(data) {
 
-// "<div class='fiveDay'>" + 
-//     "<p><strong>5-Day Forecast</strong> </p>" +
-//     "<div class='day day-one is-pulled-left'>uno</div>" +
-//     "<div class='day day-two is-pulled-left'>dos</div> " +
-//     "<div class='day day-two is-pulled-left'>tres</div> " +
-//     "<div class='day day-two is-pulled-left'>cuatro</div> " +
-//     "<div class='day day-two is-pulled-left'>cinco</div> " +
-//     "</div>"
+  return "<h3>Temperature: " + "</h3>" 
+}
+
+
+// return "<div class='fiveDay'>" + 
+  // "<p><strong>5-Day Forecast</strong> </p>" +
+  // "<div class='day day-one is-pulled-left'>uno</div>" +
+  // "<div class='day day-two is-pulled-left'>dos</div> " +
+  // "<div class='day day-two is-pulled-left'>tres</div> " +
+  // "<div class='day day-two is-pulled-left'>cuatro</div> " +
+  // "<div class='day day-two is-pulled-left'>cinco</div> " +
+  // "</div>"

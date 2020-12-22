7bbe5af13798a1fdc4386ae34f0c131080ad6b74
Ocampo <mariaocampo0513@gmail.com>
2020-12-22 15:34:22 -0800

    Added dates and icons

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,4 +1,5 @@
-var apiKey = "c6fd3ae4f6763936ec10d5be134b091f"
+var apiKey = "c6fd3ae4f6763936ec10d5be134b091f";
+
 
 $(document).ready(function (){
   $("#search").click(function(){
@@ -34,7 +35,13 @@ function ajaxcall(cityName) {
         url: `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly&appid=c6fd3ae4f6763936ec10d5be134b091f`,
         method: "GET",
       }).then(function(dataOneCall){
+        console.log(dataOneCall);
+
+        // current date using moment
+        var mcurrentDate = moment.unix(dataOneCall.current.dt);
+        var currentDate = mcurrentDate.format("MM/DD/YYYY");
         
+
         var fiveDayTitle = document.getElementById("forecast");
         fiveDayTitle.classList.remove("hide")
         
@@ -42,10 +49,10 @@ function ajaxcall(cityName) {
         var array = dataOneCall.daily;
         console.log(array);
         
-
         $(".cityName").html(
           "<div class='cityInfo sections' id='outputWeather'" +
-          "<h1 style='font-weight:bold; font-size:25px'>"+ "<div class= 'cityweather'>" + cityName + "<img class='image' src= http://openweathermap.org/img/w/" + dataCurrent.weather[0].icon + ".png>" + "</div>" + "</h1>" +
+          "<h1 style='font-weight:bold; font-size:25px'>"+ "<div class= 'cityweather'>" + cityName + currentDate + 
+          "<img class='image' src= http://openweathermap.org/img/w/" + dataCurrent.weather[0].icon + ".png>" + "</div>" + "</h1>" +
   
           "<h3> Temperature: " + dataCurrent.main.temp + " &deg;F</h3>" +
           "<h3> Humidity: " + dataCurrent.main.humidity + "%</h3>" +
@@ -59,10 +66,15 @@ function ajaxcall(cityName) {
         // var imageForecast = 
         // var dateWeek= 
           for (var i=0; i<array.length; i++){
-            
+            // forecast dates using moment
+            var mForecastDate = moment.unix(dataOneCall.daily[i].dt);
+            var forecastDate = mForecastDate.format("MM/DD/YYYY");
+
             $(".wf").html(
               "<div>"+ 
               "<div class= 'card'" +
+              "<h2>" +forecastDate + "</h2>" + 
+              "<img class='image' src= http://openweathermap.org/img/w/" + dataOneCall.daily[i].weather[0].icon + ".png>" +
               "<h3> Temperature: " + dataOneCall.daily[i].temp.day + " &deg;F</h3>" +
               "<h3> Humidity: " + dataOneCall.daily[i].humidity + "%</h3>" + 
               "</div>" +

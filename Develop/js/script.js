e47c538e7294003033aa49322608fef2abb227b8
Ocampo <mariaocampo0513@gmail.com>
2020-12-22 10:23:43 -0800

    Added one call API

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -12,7 +12,7 @@ $(document).ready(function (){
 // ajaxcall for both weather APIs requiring city name
 function ajaxcall(cityName) {
   var WeatherAPI= `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=${apiKey}`;
-  var fiveDayAPI= `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=imperial&appid=${apiKey}`;
+  // var fiveDayAPI= `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=imperial&appid=${apiKey}`;
   
   // var uvIndex = `https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=c6fd3ae4f6763936ec10d5be134b091f`;
   
@@ -21,34 +21,31 @@ function ajaxcall(cityName) {
     $.ajax({
       url: WeatherAPI,
       method: "GET",
-      success: function (data) {
-        console.log(data);  
-        
-        var lat= data.coord.lat;
-        var lon= data.coord.lon;
-        console.log(lat,lon);
-        var current = show(data)
-        
-        $("#outputWeather").html(current);
-        $("#inputCity").val("")
-      }
-    });
-    // prepend buttons for searched cities
-    var newbutton = $( ".prepend" ).prepend ("<div>" + "<li class='button is-fullwidth' >" + cityName + "</li>" + "</div>");
+     
+      
 
 
-    // Five Day forecast API ajax call
-    $.ajax({
-      url: fiveDayAPI,
-      method: "GET",
-      success: function (data) {
-        console.log(data);  
+    }).then(function(data){
+      console.log(data);  
+      var lat= data.coord.lat;
+      var lon= data.coord.lon;
+
+      var current = show(data)
+      $("#cityName").html(current);
+     
+      $.ajax({
+        url: `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly&appid=c6fd3ae4f6763936ec10d5be134b091f`,
+        method: "GET",
+      }).then(function(data){
+        console.log(data);
         var forecast = display(data) 
         $(".forecast").html(forecast);
-        // $("#inputCity").val("")
-      
-      }
-    });
+      })
+
+
+    })
+    // prepend buttons for searched cities
+    var newbutton = $( ".prepend" ).prepend ("<div>" + "<li class='button is-fullwidth' >" + cityName + "</li>" + "</div>");
 
   } else{
     $("#error").html("field cannot be empty")
@@ -64,31 +61,31 @@ $(".prepend").on("click", "li", function(){
 
 
 function show (data) {
+  console.log(data);
   return "<div class='cityInfo sections' id='outputWeather'" +
   "<h1 style='font-weight:bold; font-size:25px'>"+ "<div class= 'cityweather'>" + data.name + "<img class='image' src= http://openweathermap.org/img/w/" + data.weather[0].icon + ".png>" + "</div>" + "</h1>" +
 
   "<h3 class='secondaryfont'>Temperature: " + data.main.temp + " &deg;F</h3>" +
   "<h3 class='secondaryfont'>Humidity: " + data.main.humidity + "%</h3>" +
   "<h3 class='secondaryfont'>Wind Speed: " + data.wind.speed + " MPH </h3>" +
-  "<h3 class='secondaryfont'>UV index: " + "</h3>" +
+  "<h3 class='secondaryfont'>UV index: " + data + "</h3>" +
   "</div>";  
 }
 
 
+
+
 function display(data) {
   console.log('forecast',data);
-  return "<div class= 'message is-info'>" + 
-  "<h3 class='secondaryfont'> Temperature: " + data.list[0].main.temp + " &deg;F</h3>" +
-  "<h3 class='secondaryfont'> Humidity: " + data.list[0].main.humidity + "%</h3>" + 
-  "</div>" +
+  return "<div class= 'message is-info'>"+
+  "<h3 class='font'><strong>5-Day Forecast</strong> </h3>" + 
+  "<h3 class='secondaryfont'> Temperature: " + data.daily[0].temp.day + " &deg;F</h3>" +
+  "<h3 class='secondaryfont'> Humidity: " + data.daily[0].humidity + "%</h3>" + 
   "</div>"; 
 }
 
 
 
 
-
-// I need to display every day of the week into the screen. 
-// 
 
 
